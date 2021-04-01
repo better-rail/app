9a2a13fb47ef8475f754fbbb514bf7752ac55945
Guy Tepper <guytepper@gmail.com>
2021-04-01 19:19:38 +0300

    Initial commit

---
+++ app/components/text/text.props.ts
@@ -0,0 +1,36 @@
+import { TextStyle, TextProps as TextProperties } from "react-native"
+import { TextPresets } from "./text.presets"
+import { TxKeyPath } from "../../i18n"
+
+export interface TextProps extends TextProperties {
+  /**
+   * Children components.
+   */
+  children?: React.ReactNode
+
+  /**
+   * Text which is looked up via i18n.
+   */
+  tx?: TxKeyPath
+
+  /**
+   * Optional options to pass to i18n. Useful for interpolation
+   * as well as explicitly setting locale or translation fallbacks.
+   */
+  txOptions?: I18n.TranslateOptions
+
+  /**
+   * The text to display if not using `tx` or nested components.
+   */
+  text?: string
+
+  /**
+   * An optional style override useful for padding & margin.
+   */
+  style?: TextStyle | TextStyle[]
+
+  /**
+   * One of the different types of text presets.
+   */
+  preset?: TextPresets
+}
---
+++ app/utils/storage/async-storage.ts
@@ -0,0 +1,3 @@
+import AsyncStorage from "@react-native-async-storage/async-storage"
+
+export { AsyncStorage }
