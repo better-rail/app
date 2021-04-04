11eafceb956023bb5b091ad57fba2ed617816b93
Guy Tepper <guytepper@gmail.com>
2021-04-04 18:17:38 +0300

    Fixed station item typing

--- app/screens/select-station/useStationFiltering.ts
+++ app/screens/select-station/useStationFiltering.ts
@@ -2,12 +2,13 @@ import stations from "../../data/stations.js"
 import { ImageSourcePropType } from "react-native"
 
 type StationItem = {
-  id: number
+  id: string
   name: string
   image: ImageSourcePropType
 }
 
-export default function useStationItems(searchTerm: string): [StationItem] {
+export default function useStationItems(searchTerm: string): [StationItem?] {
+  if (searchTerm === "") return []
   return stations.filter((item) => {
     return item.name.indexOf(searchTerm) > -1
   })
