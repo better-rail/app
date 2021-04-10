e67294df639386e4c6e0995bdc7433db4cf454dd
Guy Tepper <guytepper@gmail.com>
2021-04-10 10:26:25 +0300

    Added route station card

--- app/screens/route-details/route-details-screen.tsx
+++ app/screens/route-details/route-details-screen.tsx
@@ -1,10 +1,11 @@
 import React from "react"
 import { observer } from "mobx-react-lite"
-import { View, ViewStyle } from "react-native"
+import { View, Image, ViewStyle, ImageStyle, TextStyle } from "react-native"
 import { RouteCard, RouteDetailsHeader, Screen, Text } from "../../components"
 import { RouteDetailsScreenProps } from "../../navigators/main-navigator"
 import { color, spacing } from "../../theme"
 import { SharedElement } from "react-navigation-shared-element"
+import { ScrollView } from "react-native-gesture-handler"
 
 const ROOT: ViewStyle = {
   flex: 1,
@@ -26,23 +27,60 @@ export const RouteDetailsScreen = observer(function RouteDetailsScreen({ route }
 
   return (
     <Screen style={ROOT} preset="fixed" unsafe={true}>
-      <SharedElement id="route-header">
+      <SharedElement id="route-header" style={{ zIndex: 10 }}>
         <RouteDetailsHeader
           originId={route.params.originId}
           destinationId={route.params.destinationId}
-          style={{ paddingHorizontal: spacing[3], marginBottom: spacing[3] }}
+          style={{ paddingHorizontal: spacing[3], zIndex: 10 }}
         />
       </SharedElement>
-      <View style={{ paddingHorizontal: spacing[3] }}>
-        <RouteCard
-          departureTime={departureTime}
-          arrivalTime={arrivalTime}
-          estTime={routeItem.estTime}
-          stops={stops}
-          bounceable={false}
-        />
-        <Text preset="header" text="הי!" />
-      </View>
+      <ScrollView contentContainerStyle={{ paddingTop: spacing[5] }}>
+        <RouteStationCard />
+      </ScrollView>
     </Screen>
   )
 })
+
+const railwayStationIcon = require("../../../assets/railway-station.png")
+
+const ROUTE_STATION_WRAPPER: ViewStyle = {
+  flexDirection: "row",
+  alignItems: "center",
+  paddingVertical: spacing[3],
+  paddingHorizontal: spacing[7],
+  backgroundColor: color.secondaryBackground,
+}
+
+const ROUTE_STATION_DETAILS: ViewStyle = {
+  marginStart: spacing[3],
+}
+
+const ROUTE_STATION_TIME: TextStyle = {
+  marginEnd: spacing[3],
+  fontSize: 18,
+  fontWeight: "700",
+  fontFamily: "System",
+}
+
+const ROUTE_STATION_NAME: TextStyle = {
+  marginBottom: -2,
+  marginEnd: spacing[3],
+  fontSize: 17,
+  fontWeight: "700",
+}
+
+const RAILWAY_ICON: ImageStyle = {
+  width: 42.5,
+  height: 42.5,
+}
+
+const RouteStationCard = () => (
+  <View style={ROUTE_STATION_WRAPPER}>
+    <Text style={ROUTE_STATION_TIME}>15:03</Text>
+    <Image style={RAILWAY_ICON} source={railwayStationIcon} />
+    <View style={ROUTE_STATION_DETAILS}>
+      <Text style={ROUTE_STATION_NAME}>בית יהושוע</Text>
+      <Text>רציף 3</Text>
+    </View>
+  </View>
+)
