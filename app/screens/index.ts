071537122787bc550699cc18128c9d835673a883
Guy Tepper <guytepper@gmail.com>
2021-04-19 11:35:27 +0300

    Remove unused files

--- app/screens/index.ts
+++ app/screens/index.ts
@@ -1,7 +1,3 @@
-export * from "./welcome/welcome-screen"
-export * from "./demo/demo-screen"
-export * from "./demo/demo-list-screen"
-// export other screens here
 export * from "./planner/planner-screen"
 export * from "./select-station/select-station-screen"
 export * from "./route-list/route-list-screen"
