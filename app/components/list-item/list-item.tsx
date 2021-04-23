302b546133861ba66a0a796fa34576302c710248
Guy Tepper <guytepper@gmail.com>
2021-04-23 11:03:23 +0300

    Better respond to font scale variations

--- app/components/list-item/list-item.tsx
+++ app/components/list-item/list-item.tsx
@@ -1,8 +1,21 @@
 import * as React from "react"
-import { View, Image, ViewStyle, TextStyle, ImageStyle, TouchableHighlightProps } from "react-native"
+import {
+  View,
+  Image,
+  ViewStyle,
+  TextStyle,
+  ImageStyle,
+  TouchableHighlightProps,
+  PixelRatio,
+  ImageSourcePropType,
+} from "react-native"
 import { color, spacing, typography } from "../../theme"
 import { Text } from "../"
 import { TouchableHighlight } from "react-native-gesture-handler"
+
+// #region styles
+
+const fontScale = PixelRatio.getFontScale()
 
 const chevronIcon = require("../../../assets/chevron.png")
 
@@ -13,7 +26,22 @@ const CONTAINER: ViewStyle = {
   backgroundColor: color.secondaryBackground,
   // borderColor: color.seperator,
   // borderWidth: 1,
-  padding: spacing[4],
+  paddingVertical: spacing[3] * fontScale,
+  paddingHorizontal: spacing[4],
+}
+
+const CONTENT_CONTAINER: ViewStyle = {
+  flexDirection: "row",
+  alignItems: "center",
+}
+
+const THUMB_IMAGE: ImageStyle = {
+  width: 55 * fontScale,
+  height: 55 * fontScale,
+  marginEnd: spacing[3] * fontScale,
+  borderWidth: 1,
+  borderColor: color.seperator,
+  borderRadius: 2,
 }
 
 const TEXT_CONTENT_WRAPPER: ViewStyle = {}
@@ -29,32 +57,41 @@ const SUBTITLE: TextStyle = {
   fontFamily: typography.primary,
   fontSize: 14,
   color: color.text,
+  opacity: 0.85,
 }
 
 const CHEVRON_ICON: ImageStyle = {
-  width: 8,
-  height: 16,
-  marginEnd: spacing[2],
+  width: 8 * fontScale,
+  height: 16 * fontScale,
+  marginEnd: spacing[2] * fontScale,
   tintColor: color.dim,
   opacity: 0.6,
 }
 
-export interface ListItemProps extends TouchableHighlightProps {}
+// #endregion
+
+export interface ListItemProps extends TouchableHighlightProps {
+  image: ImageSourcePropType
+}
 
 /**
  * Describe your component here
  */
 export const ListItem = function ListItem(props: ListItemProps) {
-  const { onPress, style } = props
+  const { image, onPress, style } = props
   const title = "ירושלים - יצחק נבון"
   const subtitle = "היום בשעה 08:33"
 
   return (
     <TouchableHighlight onPress={onPress} style={style} underlayColor={color.inputPlaceholderBackground}>
       <View style={CONTAINER}>
-        <View style={TEXT_CONTENT_WRAPPER}>
-          <Text style={TITLE}>{title}</Text>
-          {subtitle && <Text style={SUBTITLE}>{subtitle}</Text>}
+        <View style={CONTENT_CONTAINER}>
+          {image && <Image style={THUMB_IMAGE} source={image} />}
+
+          <View style={TEXT_CONTENT_WRAPPER}>
+            <Text style={TITLE}>{title}</Text>
+            {subtitle && <Text style={SUBTITLE}>{subtitle}</Text>}
+          </View>
         </View>
         <Image style={CHEVRON_ICON} source={chevronIcon} />
       </View>
