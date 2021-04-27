0959595af879575668157c6b1528a0f42b9e8a9c
Guy Tepper <guytepper@gmail.com>
2021-04-27 10:52:14 +0300

    Merge branch 'main' into android-dark-mode

--- app/navigators/index.ts
+++ app/navigators/index.ts
@@ -1,5 +1,4 @@
 export * from "./main-navigator"
 export * from "./root-navigator"
-export * from "./voucher-navigator"
 export * from "./navigation-utilities"
 export * from "./settings/settings-navigator"
