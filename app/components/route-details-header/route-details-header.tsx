539ec79339d2b7e641f14515cf2bc1ef8fc8c1c0
Guy Tepper <guytepper@gmail.com>
2021-05-12 19:48:38 +0300

    Clean up

--- app/components/route-details-header/route-details-header.tsx
+++ app/components/route-details-header/route-details-header.tsx
@@ -9,8 +9,8 @@ import { isRTL } from "../../i18n"
 const arrowIcon = require("../../../assets/arrow-left.png")
 
 const colorScheme = Appearance.getColorScheme()
-// #region styles
 
+// #region styles
 const ROUTE_DETAILS_WRAPPER: ViewStyle = {
   flexDirection: "row",
   justifyContent: "center",
@@ -67,27 +67,14 @@ const GARDIENT: ViewStyle = {
   top: 0,
   opacity: 1,
 }
-
 // #endregion
 
 export interface RouteDetailsHeaderProps {
-  /**
-   * The route's origin station Id.
-   */
   originId: string
-  /**
-   * The route's destination station Id.
-   */
   destinationId: string
-  /**
-   * An optional style override useful for padding & margin.
-   */
   style?: ViewStyle
 }
 
-/**
- * Describe your component here
- */
 export const RouteDetailsHeader = React.memo(function RouteDetailsHeader(props: RouteDetailsHeaderProps) {
   const { originId, destinationId, style } = props
 
