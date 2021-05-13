bb857d06ee24eec8fed93cb91475dfcdafc64379
Guy Tepper <guytepper@gmail.com>
2021-05-13 11:35:02 +0300

    Added favorites model

---
+++ app/models/favorites/favorites.ts
@@ -0,0 +1,24 @@
+import { Instance, SnapshotOut, types } from "mobx-state-tree"
+
+/**
+ * Model description here for TypeScript hints.
+ */
+export const FavoritesModel = types
+  .model("Favorites")
+  .props({})
+  .views((self) => ({})) // eslint-disable-line @typescript-eslint/no-unused-vars
+  .actions((self) => ({})) // eslint-disable-line @typescript-eslint/no-unused-vars
+
+/**
+ * Un-comment the following to omit model attributes from your snapshots (and from async storage).
+ * Useful for sensitive data like passwords, or transitive state like whether a modal is open.
+
+ * Note that you'll need to import `omit` from ramda, which is already included in the project!
+ *  .postProcessSnapshot(omit(["password", "socialSecurityNumber", "creditCardNumber"]))
+ */
+
+type FavoritesType = Instance<typeof FavoritesModel>
+export interface Favorites extends FavoritesType {}
+type FavoritesSnapshotType = SnapshotOut<typeof FavoritesModel>
+export interface FavoritesSnapshot extends FavoritesSnapshotType {}
+export const createFavoritesDefaultModel = () => types.optional(FavoritesModel, {})
