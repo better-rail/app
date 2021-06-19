2b48f5407f3fec8d093d5403d6a5a297a7eabbd1
Guy Tepper <guytepper@gmail.com>
2021-06-19 22:16:46 +0300

    💫 - Removed dynamic status bar style change
    RN's Appearance module provides inconsistent values when using the change event listener

--- app/components/screen/screen.tsx
+++ app/components/screen/screen.tsx
@@ -1,24 +1,19 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useMemo } from "react"
 import { Appearance, KeyboardAvoidingView, Platform, ScrollView, StatusBar, View } from "react-native"
 import { useSafeAreaInsets } from "react-native-safe-area-context"
 import { ScreenProps } from "./screen.props"
 import { isNonScrolling, offsets, presets } from "./screen.presets"
+
+const isDarkMode = Appearance.getColorScheme() === "dark"
 
 const isIos = Platform.OS === "ios"
 
 function ScreenWithoutScrolling(props: ScreenProps) {
   const insets = useSafeAreaInsets()
   const preset = presets.fixed
-  const [isDarkMode, setIsDarkMode] = useState(() => Appearance.getColorScheme() === "dark")
   const style = props.style || {}
   const backgroundStyle = props.backgroundColor ? { backgroundColor: props.backgroundColor } : {}
   const insetStyle = { paddingTop: props.unsafe ? 0 : insets.top }
-
-  useEffect(() => {
-    Appearance.addChangeListener(({ colorScheme }) => {
-      setIsDarkMode(colorScheme === "dark")
-    })
-  }, [])
 
   return (
     <KeyboardAvoidingView
@@ -41,15 +36,8 @@ function ScreenWithScrolling(props: ScreenProps) {
   const insets = useSafeAreaInsets()
   const preset = presets.scroll
   const style = props.style || {}
-  const [isDarkMode, setIsDarkMode] = useState(() => Appearance.getColorScheme() === "dark")
   const backgroundStyle = props.backgroundColor ? { backgroundColor: props.backgroundColor } : {}
   const insetStyle = { paddingTop: props.unsafe ? 0 : insets.top }
-
-  useEffect(() => {
-    Appearance.addChangeListener(({ colorScheme }) => {
-      setIsDarkMode(colorScheme === "dark")
-    })
-  }, [])
 
   return (
     <KeyboardAvoidingView
