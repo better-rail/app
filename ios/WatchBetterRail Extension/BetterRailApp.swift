e9a07be089e83f3c3a3929af4227b8f177161d3c
Guy Tepper <guytepper@gmail.com>
2021-10-03 17:05:03 +0300

    Initial commit

---
+++ ios/WatchBetterRail Extension/BetterRailApp.swift
@@ -0,0 +1,19 @@
+//
+//  BetterRailApp.swift
+//  WatchBetterRail Extension
+//
+//  Created by Guy Tepper on 03/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
