19f30908141b51a6fa0dbaabaaec44c91df17205
Guy Tepper <guytepper@gmail.com>
2021-10-04 15:37:52 +0300

    Replace favorites array instead of appending

--- ios/WatchBetterRail Extension/Models/FavoritesModel.swift
+++ ios/WatchBetterRail Extension/Models/FavoritesModel.swift
@@ -11,18 +11,23 @@ struct FavoritesModel {
   var routes: [FavoriteRoute] = []
   
   mutating func updateRoutesFromApplicationContext(_ applicationContext: [String: Any]) {
+    
+    var favoriteRoutes: [FavoriteRoute] = []
+    
     for (key, value) in applicationContext {
       let originId = key
       if let destinationId = value as? String,
          let originStation = getStationById(originId),
          let destinationStation = getStationById(destinationId) {
-          self.routes.append(
+          favoriteRoutes.append(
             FavoriteRoute(id: Int("\(originId)\(destinationId)")!, origin: originStation, destination: destinationStation)
           )
       } else {
           print("🚨 Couldn't extract application context")
       }
     }
+    
+    self.routes = favoriteRoutes
 
   }
 }
