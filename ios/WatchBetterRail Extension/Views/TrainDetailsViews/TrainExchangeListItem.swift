43fb67aeed92c3c4dc28113442421628832498a8
Guy Tepper <guytepper@gmail.com>
2021-10-07 12:31:19 +0300

    Changed exchange background color

--- ios/WatchBetterRail Extension/Views/TrainDetailsViews/TrainExchangeListItem.swift
+++ ios/WatchBetterRail Extension/Views/TrainDetailsViews/TrainExchangeListItem.swift
@@ -1,6 +1,5 @@
 import SwiftUI
 
-// TODO: Change background color
 // TODO: Add platform change & waiting time notices
 struct TrainExchangeListItem: View {
   let stationName: String
@@ -9,13 +8,29 @@ struct TrainExchangeListItem: View {
   
   var body: some View {
     VStack(alignment: .leading) {
-      Text("\(Image(systemName: "arrow.left.arrow.right.circle.fill")) החלפה")
-        .fontWeight(.semibold)
-        .foregroundColor(Color.blue)
+      ZStack(alignment: .leading) {
+//        Rectangle().fill(.blue)
+        Text("\(Image(systemName: "arrow.left.arrow.right.circle.fill")) החלפה")
+          .fontWeight(.semibold).foregroundColor(.accentColor)
+//          .padding(.leading, 16)
+      }
       
-      Text(time).font(Font.custom("Heebo", size: 16)).fontWeight(.bold)
-      Text(stationName).font(Font.custom("Heebo", size: 18)).fontWeight(.medium)
-      Text("רציף \(platform)")
+      VStack(alignment: .leading) {
+        Text(time).font(Font.custom("Heebo", size: 16)).fontWeight(.bold)
+        Text(stationName).font(Font.custom("Heebo", size: 18)).fontWeight(.medium)
+//        Text("רציף \(platform)")
+      }
+//      .padding(.leading, 16)
+      
     }.font(Font.custom("Heebo", size: 14))
+     .foregroundColor(.black)
+     .listRowBackground(Color.white.cornerRadius(5))
+//     .listRowInsets(EdgeInsets(top: -4, leading: -8, bottom: 0, trailing: -8))
+  }
+}
+
+struct TrainExchangeListItem_Previews: PreviewProvider {
+  static var previews: some View {
+    TrainStopListItem(time: "09:41", stationName: "ת׳׳א - סבידור מרכז")
   }
 }
