88d96c76e7c1acb8933dec33f67aeb89d53d82c6
Guy Tepper <guytepper@gmail.com>
2021-10-10 13:29:01 +0300

    ⚡️ - Enable Hermes on Android (#66)

--- app/utils/helpers/date-helpers.ts
+++ app/utils/helpers/date-helpers.ts
@@ -1,3 +1,5 @@
+import { parse } from "date-fns"
+
 /**
  * Parses a string date as formatted in the Israel Rail API.
  * We need this function since the date comes formatted as 'dd/MM/YYYY', while JavaScript parses dates using the 'MM/dd/YYYY' format.
@@ -13,7 +15,7 @@ export function parseApiDate(dateString: string) {
   if (parseInt(month) > 12) {
     throw new Error("The provided date is formatted incorrectly.\nEnsure the date uses the `dd/MM/YYYY` format.")
   }
-
-  const formattedDate = `${month}/${day}/${year} ${time}`
-  return Date.parse(formattedDate)
+  const formattedDate = `${day}/${month}/${year} ${time}`
+  const parsedDate = parse(formattedDate, "dd/MM/yyyy HH:mm:ss", new Date())
+  return parsedDate.getTime()
 }
