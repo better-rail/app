457646452b70f5b039568750aad6669152f98d47
Guy Tepper <guytepper@gmail.com>
2021-10-19 16:53:52 +0300

    Start work on english localization

--- ios/WatchBetterRail Extension/Views/RoutesView.swift
+++ ios/WatchBetterRail Extension/Views/RoutesView.swift
@@ -7,15 +7,15 @@ struct RoutesView: View {
     var body: some View {
       VStack {
         HStack {
-          Text("יציאה").foregroundColor(Color.gray)
+          Text("departure").foregroundColor(Color.gray)
           Spacer()
-          Text("הגעה").foregroundColor(Color.gray)
+          Text("arrival").foregroundColor(Color.gray)
         }.font(Font.custom("Heebo", size: 16))
         
         if (route.loading) { ProgressView().progressViewStyle(CircularProgressViewStyle())
         } else if (route.trains.count == 0) {
           Image(systemName: "tram").padding(.vertical, 2.0).font(.system(size: 24))
-          Text("לא נמצאו רכבות להיום").font(Font.custom("Heebo", size: 14))
+          Text("no-trains-found").font(Font.custom("Heebo", size: 14))
           Spacer()
         } else {
           List (0 ..< route.trains.count) { index in
