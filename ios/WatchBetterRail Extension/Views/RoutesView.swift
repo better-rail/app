fef039f6f0ad26b3fd8a38ca04dc722e20f4fbe4
Guy Tepper <guytepper@gmail.com>
2021-10-27 15:12:27 +0300

    🚸 - Display no internet connection message on Apple Watch (#74)

--- ios/WatchBetterRail Extension/Views/RoutesView.swift
+++ ios/WatchBetterRail Extension/Views/RoutesView.swift
@@ -12,11 +12,13 @@ struct RoutesView: View {
           Text("arrival").foregroundColor(Color.gray)
         }.font(Font.custom("Heebo", size: 16))
         
-        if (route.loading) { ProgressView().progressViewStyle(CircularProgressViewStyle())
+        if (route.loading) {
+          ProgressView().progressViewStyle(CircularProgressViewStyle())
+        }
+        else if let requestError = route.error {
+          InfoMessage(imageName: "wifi.exclamationmark", message: requestError.localizedDescription)
         } else if (route.trains.count == 0) {
-          Image(systemName: "tram").padding(.vertical, 2.0).font(.system(size: 24))
-          Text("no-trains-found").font(Font.custom("Heebo", size: 14))
-          Spacer()
+          InfoMessage(imageName: "tram", message: "no-trains-found")
         } else {
           List (0 ..< route.trains.count) { index in
             // TODO: Refactor train property names - too confusing!
@@ -39,7 +41,21 @@ struct RoutesView: View {
         route.shouldRefetchRoutes()
       }
     }
+}
+
+struct InfoMessage: View {
+  var imageName: String
+  var message: String
+  
+  var body: some View {
+    VStack(alignment: .center) {
+      Spacer()
+      Image(systemName: imageName).padding(.vertical, 2.0).font(.system(size: 24))
+      Text(message).font(Font.custom("Heebo", size: 14)).multilineTextAlignment(.center)
+      Spacer()
+    }
   }
+}
 
 struct RoutesView_Previews: PreviewProvider {
     static var previews: some View {
