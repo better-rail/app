9a2fd6943e5f5c50a40e0f9a66dd4cf80e493894
Guy Tepper <guytepper@gmail.com>
2021-12-18 17:24:16 +0200

    Display empty entry if there are no trains tomorrow

--- ios/BetterRailWidget/EntriesGenerator.swift
+++ ios/BetterRailWidget/EntriesGenerator.swift
@@ -18,19 +18,22 @@ struct EntriesGenerator {
     
     if (routes.today.count == 0 && routes.tomorrow.count == 0) {
       entries.append(getEmptyEntry(originId: originId, destinationId: destinationId))
-    }
-    
-    if (routes.today.count > 0) {
-      entries = generateEntriesForRoutes(routes.today, originId: originId, destinationId: destinationId)
-      lastTrainEntryDate = entries[entries.count - 1].date
-    }
-    
-    if (routes.tomorrow.count > 0) {
-      var tomorrowEntries = generateEntriesForRoutes(routes.tomorrow, originId: originId, destinationId: destinationId)
-      tomorrowEntries[0].date = lastTrainEntryDate
+    } else {
+      if (routes.today.count > 0) {
+        entries = generateEntriesForRoutes(routes.today, originId: originId, destinationId: destinationId)
+        lastTrainEntryDate = Calendar.current.date(byAdding: .minute, value: 2, to: entries[entries.count - 1].date)!
+      }
       
-      // Add first tomorrow's entry as the last entry for today
-      entries.append(tomorrowEntries[0])
+      if (routes.tomorrow.count > 0) {
+        var tomorrowEntries = generateEntriesForRoutes(routes.tomorrow, originId: originId, destinationId: destinationId)
+        tomorrowEntries[0].date = lastTrainEntryDate
+        
+        // Add tomorrow's first entry as the last entry for today
+        entries.append(tomorrowEntries[0])
+      } else {
+        // If there are no trains coming up tomorrow, display an empty entry at the end of today's entries
+        entries.append(getEmptyEntry(originId: originId, destinationId: destinationId, date: lastTrainEntryDate))
+      }
     }
         
     return entries
@@ -96,12 +99,12 @@ struct EntriesGenerator {
   }
 
   
-  func getEmptyEntry(originId: String, destinationId: String) -> TrainDetail {
+  func getEmptyEntry(originId: String, destinationId: String, date: Date = Date()) -> TrainDetail {
     let origin = getStationById(originId)!
     let destination = getStationById(destinationId)!
 
     return TrainDetail(
-      date: Date(),
+      date: date,
       departureDate: "404",
       departureTime: "404",
       arrivalTime: "404",
