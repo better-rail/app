e1d72ad8e0d3aa958af4106c23e2fd73354d8a36
Guy Tepper <guytepper@gmail.com>
2021-12-31 12:16:42 +0200

    Same station error

--- app/services/api/api.types.ts
+++ app/services/api/api.types.ts
@@ -39,6 +39,7 @@ export type RailApiGetRoutesResult = {
     Routes: { IsExchange: boolean; EstTime: string; Train: RailApiRouteItem[] }[]
     Details: { Origin: string; Destination: string; Date: string; SugKav: string; Hour: string }
     Delays: { Station: string; Date: string; Time: string; Train: string; Min: string }[]
+    Error: { Description: string; ErrorId: string; Exception: string }
   }
 }
 
