a0828b8dcbe95ae323e2724e1973264152a24dcf
Guy Tepper <hey@guytepper.com>
2022-05-04 17:56:29 +0300

    Integrate with product list

--- app/screens/settings/settings-tip-jar-screen.tsx
+++ app/screens/settings/settings-tip-jar-screen.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
 import { observer } from "mobx-react-lite"
-import { View, ViewStyle, TextStyle, Platform } from "react-native"
+import { View, ViewStyle, TextStyle, Platform, ActivityIndicator } from "react-native"
 import { useIAP } from "react-native-iap"
 import { Screen, Text } from "../../components"
 import { color, isDarkMode, spacing } from "../../theme"
@@ -30,16 +30,17 @@ const TIP_INTRO_TITLE: TextStyle = {
 }
 
 const TIP_INTRO_SUBTITLE: TextStyle = {
+  paddingHorizontal: spacing[4] + 2,
+  marginBottom: spacing[4],
   fontSize: 16.5,
   textAlign: "center",
-  marginBottom: spacing[4],
 }
 
 const LIST_ROW: ViewStyle = {
   flexDirection: "row",
   justifyContent: "space-between",
   alignItems: "center",
-  paddingHorizontal: spacing[4],
+  paddingHorizontal: spacing[3],
   paddingVertical: spacing[2] + 2,
   marginHorizontal: spacing[2],
   marginBottom: spacing[2],
@@ -68,20 +69,7 @@ const PRODUCT_IDS = ["better_rail_tip_1", "better_rail_tip_2", "better_rail_tip_
 export const TipJarScreen = observer(function TipJarScreen() {
   const [thanksModalVisible, setModalVisible] = useState(false)
 
-  const {
-    connected,
-    products,
-    promotedProductsIOS,
-    subscriptions,
-    purchaseHistories,
-    availablePurchases,
-    currentPurchase,
-    currentPurchaseError,
-    finishTransaction,
-    getProducts,
-    getAvailablePurchases,
-    getPurchaseHistories,
-  } = useIAP()
+  const { connected, products, currentPurchase, finishTransaction, requestPurchase, getProducts, getPurchaseHistories } = useIAP()
 
   useEffect(() => {
     if (connected) {
@@ -89,8 +77,18 @@ export const TipJarScreen = observer(function TipJarScreen() {
     }
   }, [connected, getProducts])
 
-  const onTipButtonPress = () => {
-    setModalVisible(true)
+  useEffect(() => {
+    getPurchaseHistories().then((history) => console.log("history!", history))
+  }, [currentPurchase])
+
+  const onTipButtonPress = async (sku: string) => {
+    try {
+      // const purchase = await requestPurchase(sku)
+      setModalVisible(true)
+      // await finishTransaction(purchase)
+    } catch (err) {
+      console.error(err)
+    }
   }
 
   return (
@@ -105,11 +103,29 @@ export const TipJarScreen = observer(function TipJarScreen() {
       <Text tx="settings.tipJarTitle" style={TIP_INTRO_TITLE} />
       <Text tx="settings.tipJarSubtitle" style={TIP_INTRO_SUBTITLE} />
 
-      <TipRow title={translate("settings.generousTip")} amount={2.99} onPress={onTipButtonPress} />
-      <TipRow title={translate("settings.amazingTip")} amount={4.99} onPress={onTipButtonPress} />
-      <TipRow title={translate("settings.massiveTip")} amount={9.99} onPress={onTipButtonPress} />
+      {products.length > 0 ? (
+        <>
+          <TipRow
+            title={translate("settings.generousTip")}
+            amount={products[0].localizedPrice}
+            onPress={() => onTipButtonPress(products[0].productId)}
+          />
+          <TipRow
+            title={translate("settings.amazingTip")}
+            amount={products[1].localizedPrice}
+            onPress={() => onTipButtonPress(products[1].productId)}
+          />
+          <TipRow
+            title={translate("settings.massiveTip")}
+            amount={products[2].localizedPrice}
+            onPress={() => onTipButtonPress(products[2].productId)}
+          />
+        </>
+      ) : (
+        <ActivityIndicator size="large" style={{ marginVertical: spacing[5] }} />
+      )}
 
-      <Text style={TOTAL_TIPS}>Total Tip: 2.99 $</Text>
+      {/* <Text style={TOTAL_TIPS}>Total Tip: 2.99 $</Text> */}
 
       <TipThanksModal isVisible={thanksModalVisible} onOk={() => setModalVisible(false)} />
     </Screen>
@@ -120,7 +136,7 @@ const TipRow = ({ title, amount, onPress }) => (
   <View style={LIST_ROW}>
     <Text>{title}</Text>
     <TouchableOpacity style={TIP_BUTTON} onPress={onPress} activeOpacity={0.6}>
-      <Text style={{ fontSize: 14, fontWeight: "500", color: color.whiteText }}>{amount} $</Text>
+      <Text style={{ fontSize: 14, fontWeight: "500", color: color.whiteText }}>{amount}</Text>
     </TouchableOpacity>
   </View>
 )
