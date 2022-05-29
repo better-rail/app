bf2d71dc003a7379604e5b2be974250c230fdddf
Guy Tepper <hey@guytepper.com>
2022-05-29 10:52:27 +0300

    Update MainActivity.java

--- android/app/src/main/java/com/betterrail/MainActivity.java
+++ android/app/src/main/java/com/betterrail/MainActivity.java
@@ -1,8 +1,13 @@
 package com.betterrail;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(null);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
