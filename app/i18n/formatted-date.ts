c91ccc93a4d31001afa8c35379c93df4ad04aec2
Matan Mashraki <mail@matanmashraki.com>
2023-04-05 08:49:48 +0300

    Fixed tomorrow day day

--- app/i18n/formatted-date.ts
+++ app/i18n/formatted-date.ts
@@ -1,9 +1,8 @@
 import { formatRelative, differenceInMinutes } from "date-fns"
 import { dateFnsLocalization, translate } from "."
 
-const now = new Date()
-
 export function useFormattedDate(date: Date) {
+  const now = new Date()
   if (differenceInMinutes(date, now) < 2) return translate("plan.now")
   return formatRelative(date, now, { locale: dateFnsLocalization })
 }
