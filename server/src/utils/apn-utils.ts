3f6d6e0adc2bebe35fea7156907988bff7e438ca
Matan Mashraki <mail@matanmashraki.com>
2023-04-11 08:55:16 +0300

    Wrapped apn promise in try catch

--- server/src/utils/apn-utils.ts
+++ server/src/utils/apn-utils.ts
@@ -48,44 +48,48 @@ export const sendApnNotification = (deviceToken: string, headers: Record<string,
   }
 
   return new Promise((resolve, reject) => {
-    const request = client.request({
-      ...mapKeys(config, (_, key) => ":" + key),
-      ...headers,
-    })
-
-    request.setEncoding("utf8")
-    if (body) {
-      request.write(JSON.stringify(body))
-    }
-
-    const response: any = []
-
-    request.on("response", (chunk) => {
-      response.push(chunk)
-    })
-
-    request.on("response", (headers) => {
-      const status = Number(headers[constants.HTTP2_HEADER_STATUS]!)
-
-      let data = ""
-
-      request.on("data", (chunk) => {
-        data += chunk
+    try {
+      const request = client.request({
+        ...mapKeys(config, (_, key) => ":" + key),
+        ...headers,
       })
 
-      request.on("end", () => {
-        if (status === 200) {
-          resolve({ status, success: true })
-        } else {
-          reject(new Error(`status: ${status}, reason: ${JSON.parse(data).reason}`))
-        }
-      })
-    })
+      request.setEncoding("utf8")
+      if (body) {
+        request.write(JSON.stringify(body))
+      }
 
-    request.on("error", (error) => {
+      const response: any = []
+
+      request.on("response", (chunk) => {
+        response.push(chunk)
+      })
+
+      request.on("response", (headers) => {
+        const status = Number(headers[constants.HTTP2_HEADER_STATUS]!)
+
+        let data = ""
+
+        request.on("data", (chunk) => {
+          data += chunk
+        })
+
+        request.on("end", () => {
+          if (status === 200) {
+            resolve({ status, success: true })
+          } else {
+            reject(new Error(`status: ${status}, reason: ${JSON.parse(data).reason}`))
+          }
+        })
+      })
+
+      request.on("error", (error) => {
+        reject(error)
+      })
+
+      request.end()
+    } catch (error) {
       reject(error)
-    })
-
-    request.end()
+    }
   })
 }
