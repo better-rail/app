872169b1a16b7d5ca583150709d200cd5887dcab
Matan Mashraki <mail@matanmashraki.com>
2023-04-15 20:34:37 +0300

    Added rate limit

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import rateLimit from "express-rate-limit"
 
 import { router } from "./routes/api"
 import { env, port } from "./data/config"
@@ -7,7 +8,16 @@ import { connectToRedis } from "./data/redis"
 import { connectToApn } from "./utils/apn-utils"
 import { scheduleExistingRides } from "./utils/ride-utils"
 
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 10,
+  standardHeaders: true,
+  legacyHeaders: false,
+  skip: () => env !== "production",
+})
+
 const app = express()
+app.use(limiter)
 app.use(express.json())
 
 app.use("/api/v1", router)
