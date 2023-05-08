196ee7fd5f1cb73ca5a6e4e60c3fb42dba36847f
Guy Tepper <hey@guytepper.com>
2023-05-08 15:50:21 +0300

    wip

--- app/hooks/use-ride-progress.tsx
+++ app/hooks/use-ride-progress.tsx
@@ -1,44 +1,40 @@
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import { differenceInMinutes } from "date-fns"
-import { findClosestStationInRoute, getTrainFromStationId } from "../utils/helpers/ride-helpers"
+import { findClosestStationInRoute, getSelectedRide, getTrainFromStationId } from "../utils/helpers/ride-helpers"
 import { RouteItem } from "../services/api"
+import { RouteApi } from "../services/api/route-api"
+import { formatDateForAPI } from "../utils/helpers/date-helpers"
+import { useQueryClient } from "react-query"
 
 type RideStatus = "waitForTrain" | "inTransit" | "inExchange" | "arrived"
 
 export function useRideProgress(route: RouteItem) {
   // initial state
-  const [status, setStatus] = useState<RideStatus>("waitForTrain")
-  const [nextStationId, setNextStationId] = useState<number>(route.trains[0].originStationId)
   const [minutesLeft, setMinutesLeft] = useState<number>(0)
-  const [delay, setDelay] = useState<number>(0)
+  const [delay, nextStationId] = useRideRoute(route)
+  const status = useRideStatus({ route, delay, nextStationId })
+
+  // update minutes left
+  useEffect(() => {
+    let minutes = 0
+    const train = getTrainFromStationId(route, nextStationId)
+
+    if (status == "inTransit") {
+      minutes = differenceInMinutes(train.arrivalTime, Date.now()) + delay
+    } else if (status == "waitForTrain" || status == "inExchange") {
+      minutes = differenceInMinutes(train.departureTime, Date.now()) + delay
+    }
+
+    setMinutesLeft(minutes)
+  }, [status, delay, nextStationId])
+
+  return [status, minutesLeft] as const
+}
+
+function useRideStatus({ route, delay, nextStationId }) {
+  const [status, setStatus] = useState<RideStatus>("waitForTrain")
 
   useEffect(() => {
-    // create a timer that runs every 1 minute
-    const timer = setInterval(() => {
-      const stationId = findClosestStationInRoute(route)
-      setNextStationId(stationId)
-
-      // fetch route
-      // const updatedRoute = ...
-
-      // update the delay
-      // setDelay(updatedRoute.delay)
-
-      // update minutes left
-      const minutes = differenceInMinutes(route.trains[route.trains.length - 1].arrivalTime, Date.now()) + delay
-      setMinutesLeft(minutes)
-    }, 60000)
-
-    const stationId = findClosestStationInRoute(route)
-    setNextStationId(stationId)
-    setDelay(route.delay)
-
-    // clear the timer when the component unmounts
-    return () => clearInterval(timer)
-  }, [])
-
-  useEffect(() => {
-    // get the current station
     const train = getTrainFromStationId(route, nextStationId)
 
     if (route.trains[0].originStationId != train.originStationId) {
@@ -48,10 +44,70 @@ export function useRideProgress(route: RouteItem) {
       }
     } else if (train.originStationId != nextStationId) {
       setStatus("inTransit")
-    } else if (Date.now() > route.trains[route.trains.length - 1].arrivalTime) {
+    } else if (Date.now() > route.trains[route.trains.length - 1].arrivalTime + delay * 60000) {
       setStatus("arrived")
     }
-  }, [nextStationId])
+  }, [route, nextStationId, delay])
 
-  return { status: "inTransit", nextStationId: 3600, minutesLeft: 14 }
+  return status
+}
+
+function useRideRoute(route: RouteItem) {
+  const queryClient = useQueryClient()
+
+  const [nextStationId, setNextStationId] = useState<number>(route.trains[0].originStationId)
+  const [delay, setDelay] = useState<number>(0)
+
+  // extract ride details from the route
+  const [originId, destinationId, date, time] = useMemo(() => {
+    const originId = route.trains[0].originStationId.toString()
+    const destinationId = route.trains[route.trains.length - 1].destinationStationId.toString()
+    const [date, time] = formatDateForAPI(route.departureTime)
+
+    return [originId, destinationId, date, time]
+  }, [route])
+
+  const refetchRoute = async () => {
+    // fetch route - needed for getting delay information
+    const routes = await new RouteApi().getRoutes(originId, destinationId, date, time)
+
+    const updatedRoute = getSelectedRide(
+      routes,
+      route.trains.map((t) => t.trainNumber),
+    )
+
+    // update the query cached routes
+    queryClient.setQueryData(
+      ["origin", originId, "destination", destinationId, "time", new Date(updatedRoute.departureTime).getDate()],
+      routes,
+    )
+
+    setDelay(updatedRoute.delay)
+  }
+
+  const updateRide = () => {
+    console.log("updating ride")
+    refetchRoute()
+
+    const stationId = findClosestStationInRoute(route)
+    setNextStationId(stationId)
+  }
+
+  useEffect(() => {
+    // set up timer
+    const timer = setInterval(() => {
+      updateRide()
+    }, 60000)
+
+    // initial refetch
+    updateRide()
+
+    const stationId = findClosestStationInRoute(route)
+    setNextStationId(stationId)
+
+    // clear the timer when the component unmounts
+    return () => clearInterval(timer)
+  }, [])
+
+  return [delay, nextStationId]
 }
