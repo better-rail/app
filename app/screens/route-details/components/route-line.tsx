f53fb1598f2d582efc74eb8bfd66f7738adebad0
Guy Tepper <hey@guytepper.com>
2023-05-11 09:00:11 +0300

    Update route-line.tsx

--- app/screens/route-details/components/route-line.tsx
+++ app/screens/route-details/components/route-line.tsx
@@ -1,7 +1,9 @@
-import { View, ViewStyle } from "react-native"
+import { View, ViewProps, ViewStyle } from "react-native"
 import { color, fontScale } from "../../../theme"
 import Animated from "react-native-reanimated"
 import { useRideProgressAnimation } from "../../../hooks/use-ride-progress"
+
+export type RouteLineStateType = "idle" | "inProgress" | "passed"
 
 const ROUTE_STOP_LINE: ViewStyle = {
   width: 4,
@@ -19,15 +21,20 @@ export const ROUTE_LINE_STATE_COLORS = {
   passed: color.greenText,
 }
 
-export const RouteLine = ({ state = "idle", style }: { state: "idle" | "inProgress" | "passed" }) => {
+interface RouteLineProps {
+  state?: RouteLineStateType
+  style?: ViewStyle
+}
+
+export const RouteLine = ({ state = "idle", style }: RouteLineProps) => {
   if (state === "inProgress") {
     return <InProgressLine />
   }
 
-  return <View style={[ROUTE_STOP_LINE, { backgroundColor: ROUTE_LINE_STATE_COLORS[state] }]} />
+  return <View style={[ROUTE_STOP_LINE, { backgroundColor: ROUTE_LINE_STATE_COLORS[state] }, style]} />
 }
 
-const InProgressLine = () => {
+const InProgressLine = ({ style }: { style?: any }) => {
   const animatedStyle = useRideProgressAnimation()
-  return <Animated.View style={[ROUTE_STOP_LINE, animatedStyle]} />
+  return <Animated.View style={[ROUTE_STOP_LINE, animatedStyle, style]} />
 }
