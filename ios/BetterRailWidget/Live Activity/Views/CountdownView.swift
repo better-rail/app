036b5421591945198b2d4609e61df780b8aa7bd2
Guy Tepper <hey@guytepper.com>
2023-05-18 16:00:17 +0300

    update delay colors

--- ios/BetterRailWidget/Live Activity/Views/CountdownView.swift
+++ ios/BetterRailWidget/Live Activity/Views/CountdownView.swift
@@ -1,17 +1,27 @@
 import SwiftUI
 
 struct CountdownView: View {
-    let targetDate: Date
-    let delay: Int
+  let targetDate: Date
+  let delay: Int
   
-    var minutesLeft: Int {
-      getMinutesLeft(targetDate: targetDate.addDelay(delay))
+  var minutesLeft: Int {
+    getMinutesLeft(targetDate: targetDate.addDelay(delay))
+  }
+  
+  var color: Color {
+    if (delay >= 5) {
+      return .red
+    } else if (delay > 0) {
+      return Color(uiColor: .label)
+    } else {
+      return .green
     }
+  }
     
-    var body: some View {
-      HStack (alignment: .firstTextBaseline, spacing: 4.0) {
-        Text(String(minutesLeft)).font(.system(size: 28, weight: .heavy, design: .rounded))
-        Text("min").font(.system(size: 20, weight: .bold, design: .rounded))
-      }.foregroundColor(delay > 0 ? .red : .green)
-    }
+  var body: some View {
+    HStack (alignment: .firstTextBaseline, spacing: 4.0) {
+      Text(String(minutesLeft)).font(.system(size: 28, weight: .heavy, design: .rounded))
+      Text("min").font(.system(size: 20, weight: .bold, design: .rounded))
+    }.foregroundColor(color)
+  }
 }
