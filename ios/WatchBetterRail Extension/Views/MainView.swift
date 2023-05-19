2e3db770e30a36326d3c81195d8c9eaaba510ddf
Guy Tepper <hey@guytepper.com>
2023-05-19 08:47:44 +0300

    update search route button placement & styles

--- ios/WatchBetterRail Extension/Views/MainView.swift
+++ ios/WatchBetterRail Extension/Views/MainView.swift
@@ -4,11 +4,23 @@ struct MainView: View {
     var body: some View {
       VStack {
         List {
-          NavigationLink("search route") {
-            SearchView()
-          }
-          
           FavoritesView(favorites: FavoritesViewModel())
+          
+          NavigationLink(destination: SearchView()) {
+            HStack(alignment: .center) {
+              VStack {
+                Text("search route")
+                  .frame(maxWidth: .infinity)
+                  .multilineTextAlignment(.center)
+              }
+            }
+          }.frame(idealHeight: 75)
+            .listRowBackground(
+              Color("midnightBlue")
+                .clipped()
+                .cornerRadius(10)
+            )
+
         }
         .listStyle(CarouselListStyle())
       }
