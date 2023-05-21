381893cf9de4100372d9a38655dc59698ec06827
Guy Tepper <hey@guytepper.com>
2023-05-21 15:28:06 +0300

    cast API values from Int to String to make localization work

--- ios/WatchBetterRail Extension/Views/RoutesView.swift
+++ ios/WatchBetterRail Extension/Views/RoutesView.swift
@@ -5,49 +5,39 @@ struct RoutesView: View {
   @ObservedObject var route: RouteViewModel
   
     var body: some View {
-      ScrollViewReader { proxy in
-        VStack {
-          HStack {
-            Text("departure").foregroundColor(Color.gray)
-            Spacer()
-            Text("arrival").foregroundColor(Color.gray)
-          }.font(Font.custom("Heebo", size: 16))
-          
-          if (route.loading) {
-            ProgressView().progressViewStyle(CircularProgressViewStyle())
-          }
-          else if let requestError = route.error {
-            InfoMessage(imageName: "wifi.exclamationmark", message: requestError.localizedDescription)
-          } else if (route.trains.count == 0) {
-            InfoMessage(imageName: "tram", message: "no-trains-found")
-          } else {
-            List (0 ..< route.trains.count, id: \.self) { index in
-              // TODO: Refactor train property names - too confusing!
-              let trainDetails = route.trains[index]
-              let trainDetailsView = TrainDetailsView(trainRoute: trainDetails)
-              
-              NavigationLink(destination: trainDetailsView) {
-                HStack {
-                  Text(formatRouteHour(trainDetails.departureTime))
-                  Spacer()
-                  Image(systemName: "arrow.forward")
-                  Spacer()
-                  Text(formatRouteHour(trainDetails.arrivalTime))
-                }
+      VStack {
+        HStack {
+          Text("departure").foregroundColor(Color.gray)
+          Spacer()
+          Text("arrival").foregroundColor(Color.gray)
+        }.font(Font.custom("Heebo", size: 16))
+        
+        if (route.loading) {
+          ProgressView().progressViewStyle(CircularProgressViewStyle())
+        }
+        else if let requestError = route.error {
+          InfoMessage(imageName: "wifi.exclamationmark", message: requestError.localizedDescription)
+        } else if route.trains.count == 0 {
+          InfoMessage(imageName: "tram", message: String(localized: "no-trains-found"))
+        } else {
+          List (0 ..< route.trains.count, id: \.self) { index in
+            // TODO: Refactor train property names - too confusing!
+            let trainDetails = route.trains[index]
+            let trainDetailsView = TrainDetailsView(trainRoute: trainDetails)
+            
+            NavigationLink(destination: trainDetailsView) {
+              HStack {
+                Text(formatRouteHour(trainDetails.departureTime))
+                Spacer()
+                Image(systemName: "arrow.forward")
+                Spacer()
+                Text(formatRouteHour(trainDetails.arrivalTime))
               }
-              .id(index)
             }
-            .listStyle(CarouselListStyle()).environment(\.defaultMinListRowHeight, 50)
-            .onReceive(route.$closestIndexToDate, perform: { index in
-              if let index = index {
-                proxy.scrollTo(index, anchor: .top)
-              }
-            })
-          }
+          }.listStyle(CarouselListStyle()).environment(\.defaultMinListRowHeight, 50)
         }
-        .onAppear {
-          route.shouldRefetchRoutes()
-        }
+      }.onAppear {
+        route.shouldRefetchRoutes()
       }
     }
 }
@@ -59,8 +49,16 @@ struct InfoMessage: View {
   var body: some View {
     VStack(alignment: .center) {
       Spacer()
-      Image(systemName: imageName).padding(.vertical, 2.0).font(.system(size: 24))
-      Text(message).font(Font.custom("Heebo", size: 14)).multilineTextAlignment(.center)
+      
+      Image(systemName: imageName)
+        .font(.title2)
+        .padding(.bottom, 2.0)
+      
+      Text(message)
+        .font(Font.custom("Heebo", size: 16))
+        .multilineTextAlignment(.center)
+        .padding(.horizontal, 8.0)
+      
       Spacer()
     }
   }
