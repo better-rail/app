05be33392564e4c80c8250408bff312889948235
Guy Tepper <hey@guytepper.com>
2023-06-03 08:28:31 +0300

    Update bottom-alert-sheet.tsx

--- app/components/sheets/bottom-alert-sheet.tsx
+++ app/components/sheets/bottom-alert-sheet.tsx
@@ -1,5 +1,28 @@
-interface BottomSheetProps {}
+import { forwardRef, useCallback, useMemo } from "react"
+import { ViewStyle } from "react-native"
+import BottomSheet, { BottomSheetBackdrop, BottomSheetProps } from "@gorhom/bottom-sheet"
 
-export function BottomSheet() {
-  return
+interface BottomSheetModalProps extends BottomSheetProps {
+  children: React.ReactNode
+  style: ViewStyle
 }
+
+export const BottomSheetModal = forwardRef<BottomSheet, BottomSheetModalProps>(({ children, ...rest }, ref) => {
+  const snapPoints = useMemo(() => ["55%"], [])
+
+  const renderBackdrop = useCallback((props) => <BottomSheetBackdrop {...props} disappearsOnIndex={-1} appearsOnIndex={2} />, [])
+
+  return (
+    <BottomSheet
+      ref={ref}
+      index={-1}
+      enablePanDownToClose
+      snapPoints={snapPoints}
+      backdropComponent={renderBackdrop}
+      handleComponent={null}
+      {...rest}
+    >
+      {children}
+    </BottomSheet>
+  )
+})
