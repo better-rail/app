12ae3476397fc4f6ed64d0bc737e3bd122df98c8
Guy Tepper <hey@guytepper.com>
2023-06-04 10:54:53 +0300

    wip

--- app/screens/live-announcement/live-announcement-screen.tsx
+++ app/screens/live-announcement/live-announcement-screen.tsx
@@ -1,19 +1,23 @@
-import { TextStyle, View } from "react-native"
-import { Screen, Text } from "../../components"
+import { Image, ImageStyle, ScrollView, TextStyle, View } from "react-native"
+import { Button, Screen, Text } from "../../components"
 import { LiveAnnouncementBackground } from "./live-announcement-bg"
 import { color, spacing } from "../../theme"
+import { translate } from "../../i18n"
 
 const SUB_TITLE: TextStyle = {
   color: color.whiteText,
   fontSize: 20,
   textAlign: "center",
   marginBottom: -4,
+  fontWeight: "500",
 }
 
 const TITLE: TextStyle = {
   color: color.whiteText,
   fontSize: 42,
   textAlign: "center",
+  marginBottom: spacing[2],
+  fontWeight: "800",
 }
 
 const TEXT: TextStyle = {
@@ -22,17 +26,32 @@ const TEXT: TextStyle = {
   color: color.whiteText,
 }
 
+const LIVE_ACTIVITY_IMAGE: ImageStyle = {
+  width: "100%",
+  height: 155,
+  resizeMode: "contain",
+  marginVertical: spacing[6],
+}
+
+const LIVE_ACTIVITY = require("../../../assets/live-activity/live-activity-hebrew.png")
+
 export function LiveAnnouncementScreen() {
   return (
-    <Screen preset="scroll" unsafe={true} statusBar="light-content">
-      <LiveAnnouncementBackground />
+    <Screen unsafe={true} statusBar="light-content">
+      <ScrollView contentContainerStyle={{ flex: 1, paddingHorizontal: spacing[5] }}>
+        <LiveAnnouncementBackground />
 
-      <View style={{ marginTop: spacing[6], paddingHorizontal: spacing[5] }}>
-        <Text tx="liveAnnounce.firstPage.subtitle" style={SUB_TITLE} />
-        <Text tx="liveAnnounce.firstPage.title" preset="header" style={TITLE} />
-        <Text tx="liveAnnounce.firstPage.description" style={TEXT} />
-        <Text tx="liveAnnounce.firstPage.weMadeAGuide" style={TEXT} />
-      </View>
+        <View style={{ marginTop: spacing[7] }}>
+          <Text tx="liveAnnounce.firstPage.subtitle" style={SUB_TITLE} />
+          <Text tx="liveAnnounce.firstPage.title" preset="header" style={TITLE} />
+          <Text tx="liveAnnounce.firstPage.description" style={TEXT} />
+          <Image source={LIVE_ACTIVITY} style={LIVE_ACTIVITY_IMAGE} />
+          <Text tx="liveAnnounce.firstPage.weMadeAGuide" style={TEXT} />
+        </View>
+        <View style={{ flex: 1 }} />
+
+        <Button title={translate("common.next")} style={{ maxHeight: 60 }} onPress={() => {}} />
+      </ScrollView>
     </Screen>
   )
 }
