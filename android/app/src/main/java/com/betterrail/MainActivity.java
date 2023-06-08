33254c073857875a2aad5c1500183c11655616b3
Matan Mashraki <mail@matanmashraki.com>
2023-06-08 17:41:35 +0300

    Fixed react-native-screens crash

--- android/app/src/main/java/com/betterrail/MainActivity.java
+++ android/app/src/main/java/com/betterrail/MainActivity.java
@@ -1,5 +1,6 @@
 package com.betterrail;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -14,6 +15,14 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "BetterRail";
+  }
+
+  /**
+   * Fix for react-native-screens (https://github.com/software-mansion/react-native-screens#android)
+   */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
