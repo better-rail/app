5d646bc75f221e53f9c2a876749a28bfe45d5953
Matan Mashraki <mail@matanmashraki.com>
2023-06-10 17:03:20 +0300

    Fixed merge mistake

--- android/app/src/main/java/com/betterrail/MainActivity.java
+++ android/app/src/main/java/com/betterrail/MainActivity.java
@@ -1,5 +1,6 @@
 package com.betterrail;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
