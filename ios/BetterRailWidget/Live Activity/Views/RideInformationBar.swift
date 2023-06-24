d3c1b3301a62ed5920a4af4fd3d2ce35ac28f4e0
Guy Tepper <hey@guytepper.com>
2023-06-24 12:15:41 +0530

    reduce spacing between content and information bar

--- ios/BetterRailWidget/Live Activity/Views/RideInformationBar.swift
+++ ios/BetterRailWidget/Live Activity/Views/RideInformationBar.swift
@@ -86,7 +86,7 @@ struct RideInformationBar: View {
           }
         Spacer()
       }
-      .padding(.top, placement == .lockScreen ? 10 : -2.0)
+      .padding(.top, placement == .lockScreen ? 10 : 0)
       .padding(.leading, placement == .lockScreen ? 0 : 4)
       .foregroundColor(placement == .dynamicIsland ? .white : Color(uiColor: .darkText))
     }
