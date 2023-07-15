edc29b2b02b8aec24bc3b13fdba87c083510b8b6
Guy Tepper <hey@guytepper.com>
2023-07-15 15:24:34 +0300

    wip

--- app/screens/live-announcement/activity-announcement-screen.android.tsx
+++ app/screens/live-announcement/activity-announcement-screen.android.tsx
@@ -16,43 +16,34 @@ const TEXT: TextStyle = { fontSize: 22, textAlign: "center" }
 
 const LIVE_ACTIVITY_IMAGE: ImageStyle = {
   width: "100%",
-  height: 155,
+  height: 355,
   resizeMode: "contain",
 }
 
+const NOTIFICATION_IMAGE = require("../../../assets/live-ride/-live-ride-notification.png")
+
 export function ActivityAnnouncementScreen({ navigation }: LiveAnnouncementStackProps) {
   const insets = useSafeAreaInsets()
-  const LIVE_ACTIVITY =
-    userLocale === "he"
-      ? require("../../../assets/live-activity/live-activity-delay-hebrew.png")
-      : require("../../../assets/live-activity/live-activity-delay-english.png")
 
   return (
     <ScrollView contentContainerStyle={{ height: "100%", paddingTop: spacing[4], backgroundColor: color.tertiaryBackground }}>
-      <View style={{ flex: 1, alignItems: "center", paddingHorizontal: spacing[2] }}>
+      <View style={{ flex: 1, alignItems: "center", paddingHorizontal: spacing[3], gap: spacing[4] }}>
         <Text tx="liveAnnounce.liveActivity.title" preset="header" style={TITLE} maxFontSizeMultiplier={1.1} />
         <Text tx="liveAnnounce.liveActivity.description" style={TEXT} maxFontSizeMultiplier={1.1} />
-        <View
-          style={{
-            shadowColor: "#333",
-            shadowOffset: { height: 0, width: 0 },
-            shadowOpacity: 0.2,
-            shadowRadius: 3,
-            marginVertical: spacing[5],
-          }}
-        >
-          <Image source={LIVE_ACTIVITY} style={LIVE_ACTIVITY_IMAGE} />
-        </View>
-        <Text tx="liveAnnounce.liveActivity.tip" style={TEXT} maxFontSizeMultiplier={1.1} />
+
+        <Image source={NOTIFICATION_IMAGE} style={LIVE_ACTIVITY_IMAGE} />
+        <Text
+          tx="liveAnnounce.liveActivity.tip"
+          style={[TEXT, { fontSize: 20, marginBottom: spacing[2] }]}
+          maxFontSizeMultiplier={1.1}
+        />
+
+        <Button
+          onPress={() => navigation.navigate("liveActivity")}
+          containerStyle={{ maxHeight: 60, width: "90%" }}
+          title={translate("common.next")}
+        />
       </View>
-
-      <View style={{ flex: 1 }} />
-
-      <Button
-        onPress={() => navigation.navigate("liveActivity")}
-        containerStyle={{ maxHeight: 60, width: "90%" }}
-        title={translate("common.next")}
-      />
     </ScrollView>
   )
 }
