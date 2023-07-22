78f1ba9a0979b8dd48c6301074b807013cb3435c
Guy Tepper <hey@guytepper.com>
2023-07-22 11:30:15 +0300

    refactor announcement card
    also changed RN Text component to our own Text component

--- app/components/announcements/announcement-card.tsx
+++ app/components/announcements/announcement-card.tsx
@@ -1,19 +1,18 @@
-import { ViewStyle, Platform, View, Text, TextStyle } from "react-native"
+import { ViewStyle, Platform, View, TextStyle } from "react-native"
 import TouchableScale from "react-native-touchable-scale"
 import { fontScale, spacing, color } from "../../theme"
+import { Text } from "../text/text"
 import { openLink } from "../../utils/helpers/open-link"
 
 const ANNOUNCEMENT_CARD: ViewStyle = {
-  maxWidth: "100%",
-  minWidth: "100%",
   minHeight: 80 * fontScale,
-  backgroundColor: color.inputBackground,
+
   marginBottom: spacing[4],
-  paddingTop: spacing[3],
-  paddingBottom: spacing[2],
-  paddingStart: spacing[3],
-  paddingEnd: spacing[4],
+  paddingVertical: spacing[3],
+  paddingHorizontal: spacing[3],
+
   borderRadius: Platform.select({ ios: 12, android: 8 }),
+  backgroundColor: color.inputBackground,
   shadowColor: color.palette.black,
   shadowOffset: { height: 0, width: 0 },
   shadowOpacity: 0.05,
@@ -23,12 +22,13 @@ const ANNOUNCEMENT_CARD: ViewStyle = {
 const TITLE_STYLE: TextStyle = {
   fontFamily: "Heebo",
   color: color.primary,
-  fontSize: fontScale * 16,
+  fontSize: 16,
+  marginBottom: spacing[0],
 }
 
 const BODY_STYLE: TextStyle = {
   fontFamily: "Heebo",
-  fontSize: fontScale * 14,
+  fontSize: 14,
 }
 
 type AnnouncementCardProps = {
@@ -37,13 +37,11 @@ type AnnouncementCardProps = {
   link?: string
 }
 
-export const AnnouncementCard: React.FC<AnnouncementCardProps> = ({ title, body, link }) => {
-  return (
-    <TouchableScale activeScale={0.98} friction={10} disabled={!link} onPress={() => openLink(link)}>
-      <View style={ANNOUNCEMENT_CARD}>
-        <Text style={TITLE_STYLE}>{title}</Text>
-        <Text style={BODY_STYLE}>{body}</Text>
-      </View>
-    </TouchableScale>
-  )
-}
+export const AnnouncementCard = ({ title, body, link }: AnnouncementCardProps) => (
+  <TouchableScale activeScale={0.98} friction={10} disabled={!link} onPress={() => openLink(link)}>
+    <View style={ANNOUNCEMENT_CARD}>
+      <Text style={TITLE_STYLE}>{title}</Text>
+      <Text style={BODY_STYLE}>{body}</Text>
+    </View>
+  </TouchableScale>
+)
