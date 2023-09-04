dd946d658f85d64121927282a4e113afa1ad07ca
Guy Tepper <hey@guytepper.com>
2023-09-04 16:06:56 -0400

    added announcements error message

--- app/components/announcements/announcements-list.tsx
+++ app/components/announcements/announcements-list.tsx
@@ -1,10 +1,12 @@
 import React from "react"
-import { ActivityIndicator } from "react-native"
+import { ActivityIndicator, Image, TextStyle, View, ViewStyle } from "react-native"
 import { useQuery } from "react-query"
 import { userLocale } from "../../i18n"
 import { Announcement, PopUpMessage, railApi } from "../../services/api"
-import { spacing } from "../../theme"
+import { color, spacing } from "../../theme"
 import { AnnouncementCard } from "./announcement-card"
+import { Text } from "../text/text"
+import { Button } from "../button/button"
 
 type AnnouncementsListProps = {
   updatesType: "regular" | "urgent"
@@ -13,21 +15,27 @@ type AnnouncementsListProps = {
 }
 
 export const AnnouncementsList: React.FC<AnnouncementsListProps> = ({ updatesType, relevantStationIds, hideLoader }) => {
-  const { isLoading, data: announcements } = useQuery<Announcement[] | PopUpMessage[]>(
-    ["announcements", updatesType, relevantStationIds],
-    () => {
-      return updatesType === "regular"
-        ? railApi.getAnnouncements(userLocale, relevantStationIds)
-        : railApi.getPopupMessages(userLocale)
-    },
-  )
+  const {
+    isLoading,
+    data: announcements,
+    isError,
+    refetch,
+  } = useQuery<Announcement[] | PopUpMessage[]>(["announcements", updatesType, relevantStationIds], () => {
+    return updatesType === "regular"
+      ? railApi.getAnnouncements(userLocale, relevantStationIds)
+      : railApi.getPopupMessages(userLocale)
+  })
 
-  if (isLoading) {
-    if (hideLoader) {
-      return <></>
-    }
+  // if (isLoading) {
+  //   if (hideLoader) {
+  //     return <></>
+  //   }
 
-    return <ActivityIndicator style={{ marginTop: spacing[5] }} size="large" />
+  //   return <ActivityIndicator style={{ marginTop: spacing[5] }} size="large" />
+  // }
+
+  if (true) {
+    return <ErrorMessage refetch={refetch} />
   }
 
   return (
@@ -42,5 +50,30 @@ export const AnnouncementsList: React.FC<AnnouncementsListProps> = ({ updatesTyp
         }
       })}
     </>
+  )
+}
+
+const ERORR_MESSAGE_WRAPPER: ViewStyle = {
+  paddingHorizontal: spacing[3],
+  alignItems: "center",
+}
+
+const ERORR_MESSAGE_TEXT: TextStyle = {
+  fontSize: 20,
+  fontWeight: "bold",
+  marginBottom: spacing[1],
+}
+
+function ErrorMessage({ refetch }) {
+  return (
+    <View style={ERORR_MESSAGE_WRAPPER}>
+      <Image source={require("../../../assets/info.png")} style={{ marginBottom: spacing[0], tintColor: color.error }} />
+      <Text style={ERORR_MESSAGE_TEXT}>Bad News</Text>
+      <Text style={{ textAlign: "center", marginBottom: spacing[3] }}>
+        There was an error loading updates from Israel Railways.
+      </Text>
+
+      <Button onPress={() => refetch()} title="Retry" containerStyle={{ width: 200 }} />
+    </View>
   )
 }
