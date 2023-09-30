479b6ac7f3149bd082441343247a9c8ac119899b
Guy Tepper <hey@guytepper.com>
2023-09-30 18:30:04 -0300

    ⬇️ - Revert `react-native-reanimated` from v3 to v2 (#325)
    seems like v3 caused the app to freeze 🥶
    
    also downgraded all library upgrades that took place on
    https://github.com/better-rail/app/pull/289

--- app/screens/planner/Important-announcement-bar.tsx
+++ app/screens/planner/Important-announcement-bar.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react"
 import { Dimensions, TextStyle, TouchableOpacity } from "react-native"
-import Animated, { useSharedValue, withDelay, withSpring, withTiming } from "react-native-reanimated"
+import Animated, { useAnimatedStyle, useSharedValue, withDelay, withTiming } from "react-native-reanimated"
 import { color, fontScale } from "../../theme"
 import { useNavigation } from "@react-navigation/native"
 import analytics from "@react-native-firebase/analytics"
@@ -22,9 +22,8 @@ export function ImportantAnnouncementBar({ title }: { title: string }) {
   const opacity = useSharedValue(0)
 
   const onStart = () => {
-    width.value = withSpring(barWidth, {
+    width.value = withTiming(barWidth, {
       duration: 1000,
-      dampingRatio: 1.4,
     })
 
     opacity.value = withDelay(650, withTiming(1, { duration: 400 }))
@@ -39,21 +38,31 @@ export function ImportantAnnouncementBar({ title }: { title: string }) {
     onStart()
   }, [])
 
+  const WrapperAnimatedStyle = useAnimatedStyle(() => ({
+    width: width.value,
+  }))
+
+  const TextAnimatedStyle = useAnimatedStyle(() => ({
+    opacity: opacity.value,
+  }))
+
   return (
     <AnimatedTouchable
       activeOpacity={0.75}
-      style={{
-        width,
-        height: fontScale > 1 ? 40 : 32,
-        marginTop: 12,
-        justifyContent: "center",
-        paddingHorizontal: 10,
-        backgroundColor: "#e74c3c",
-        borderRadius: 10,
-      }}
+      style={[
+        {
+          height: fontScale > 1 ? 40 : 32,
+          marginTop: 12,
+          justifyContent: "center",
+          paddingHorizontal: 10,
+          backgroundColor: "#e74c3c",
+          borderRadius: 10,
+        },
+        WrapperAnimatedStyle,
+      ]}
       onPress={navigateToAnnouncements}
     >
-      <Animated.Text style={[TEXT_STYLE, { opacity }]} maxFontSizeMultiplier={1.15}>
+      <Animated.Text style={[TEXT_STYLE, TextAnimatedStyle]} maxFontSizeMultiplier={1.15}>
         {title}
       </Animated.Text>
     </AnimatedTouchable>
