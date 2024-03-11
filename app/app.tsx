691f6fa419a3bbce8beb245f13997b45d1bbbd45
Guy Tepper <hey@guytepper.com>
2024-03-11 10:00:12 +0200

    remove unused import

--- app/app.tsx
+++ app/app.tsx
@@ -48,7 +48,7 @@ if (__DEV__) {
 // stack navigation, use `createNativeStackNavigator` in place of `createStackNavigator`:
 // https://github.com/kmagiera/react-native-screens#using-native-stack-navigator
 import { enableScreens } from "react-native-screens"
-import { canRunLiveActivities, monitorLiveActivities } from "./utils/ios-helpers"
+import { monitorLiveActivities } from "./utils/ios-helpers"
 import { useDeepLinking } from "./hooks/use-deep-linking"
 import { openActiveRide } from "./utils/helpers/ride-helpers"
 enableScreens()
