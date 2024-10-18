3577cf2692728e3f9072e58709f27a1b7b40d476
Guy Tepper <hey@guytepper.com>
2024-10-18 15:59:04 +0300

    hide seen urgent messages

--- app/components/announcements/urgent-announcements.tsx
+++ app/components/announcements/urgent-announcements.tsx
@@ -1,19 +1,32 @@
-import React from "react"
+import { useEffect, useState } from "react"
 import { Platform, View } from "react-native"
 import { useQuery } from "react-query"
+import { uniqBy } from "lodash"
 import { userLocale } from "../../i18n"
 import { PopUpMessage, railApi } from "../../services/api"
 import { spacing } from "../../theme"
-import { Text } from "../text/text"
-import { Screen } from "../screen/screen"
+import { Screen, Text } from ".."
 import { useIsDarkMode } from "../../hooks"
-import { AnnouncementCard } from "./announcement-card"
-import { removeHtmlTagsAndEntities } from "./announcements-utils"
-import { uniqBy } from "lodash"
+import { useStores } from "../../models"
 
 export const UrgentAnnouncements = () => {
+  const { settings } = useStores()
   const isDarkMode = useIsDarkMode()
+
   const { data: messages } = useQuery<PopUpMessage[]>(["announcements", "urgent"], () => railApi.getPopupMessages(userLocale))
+  const [unseenUrgentMessages, setUnseenUrgentMessages] = useState<PopUpMessage[]>([])
+
+  useEffect(() => {
+    const unseenUrgentMessages = settings.filterUnseenUrgentMessages(messages)
+    setUnseenUrgentMessages(unseenUrgentMessages)
+
+    if (unseenUrgentMessages) {
+      // Delay to avoid hiding the urgent announcement bar while the modal is opening
+      setTimeout(() => {
+        settings.setSeenUrgentMessagesIds(unseenUrgentMessages.map((message) => message.id))
+      }, 1000)
+    }
+  }, [messages])
 
   return (
     <Screen unsafe statusBar={Platform.select({ ios: "light-content" })} statusBarBackgroundColor={isDarkMode ? "#000" : "#fff"}>
