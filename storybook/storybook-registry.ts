a9e95cba863c40887fe8b8b901d017e11a7f05ce
Guy Tepper <hey@guytepper.com>
2025-01-05 21:28:28 +0200

    🗑️  - Remove storybook & other scaffold files (#399)

--- storybook/storybook-registry.ts
+++
@@ -1,5 +0,0 @@
-require("../app/components/text/text.story")
-require("../app/components/button/button.story")
-require("../app/components/dummy-input/dummy-input.story")
-require("../app/components/button/button.story")
-require("../app/components/station-card/station-card.story")
