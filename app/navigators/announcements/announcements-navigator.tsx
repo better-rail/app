d89fcac6fdc1138816f26c9ad6dc42815d8fc5b9
Guy Tepper <hey@guytepper.com>
2025-02-23 10:38:42 +0200

    Fix close button spacing

--- app/navigators/announcements/announcements-navigator.tsx
+++ app/navigators/announcements/announcements-navigator.tsx
@@ -51,7 +51,12 @@ export const AnnouncementsNavigator = () => (
             />
           </Pressable>
         ),
-        headerLeft: () => <CloseButton onPress={() => navigation.goBack()} style={{ marginRight: spacing[2] }} />,
+        headerLeft: () => (
+          <CloseButton
+            onPress={() => navigation.goBack()}
+            style={{ marginRight: Platform.select({ ios: spacing[2], android: spacing[5] }) }}
+          />
+        ),
         headerTitleStyle: Platform.select({ ios: iOSTitleStyle, android: { ...androidTitleStyle, marginBottom: 10 } }),
       })}
     />
