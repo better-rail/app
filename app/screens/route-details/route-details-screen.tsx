af96423b42b7e16e74a1fa83954a96f278d4880d
Matan Mashraki <12946462+planecore@users.noreply.github.com>
2025-04-18 11:39:09 +0300

    🐛 - Added `observer` to back to `RouteDetailsScreen`
    The missing `observer` (which got removed in a previous PR by mistake) caused the screen not to rerender when live ride has been updated.
    
    Co-authored-by: Guy Tepper <hey@guytepper.com>

--- app/screens/route-details/route-details-screen.tsx
+++ app/screens/route-details/route-details-screen.tsx
@@ -27,6 +27,7 @@ import type BottomSheet from "@gorhom/bottom-sheet"
 import { useStations } from "../../data/stations"
 import { calculateDelayedTime } from "../../utils/helpers/date-helpers"
 import { useSafeAreaInsets } from "react-native-safe-area-context"
+import { observer } from "mobx-react-lite"
 
 const ROOT: ViewStyle = {
   flex: 1,
@@ -42,7 +43,7 @@ const STATION_CONTAINER: ViewStyle = {
   backgroundColor: color.background,
 }
 
-export function RouteDetailsScreen({ route }: RouteDetailsScreenProps) {
+export const RouteDetailsScreen = observer(function RouteDetailsScreen({ route }: RouteDetailsScreenProps) {
   const { ride } = useStores()
   const allStations = useStations()
   const permissionSheetRef = useRef<BottomSheet>(null)
@@ -313,4 +314,4 @@ export function RouteDetailsScreen({ route }: RouteDetailsScreenProps) {
       {Platform.OS === "android" && <LivePermissionsSheet onDone={onDoneLivePermissionsSheet} ref={permissionSheetRef} />}
     </>
   )
-}
+})
