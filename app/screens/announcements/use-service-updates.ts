b8649bc527ce787f1349ad744103af82fe805c34
Guy Tepper <hey@guytepper.com>
2025-05-04 17:12:03 +0300

    Migrate firestore for real!

--- app/screens/announcements/use-service-updates.ts
+++ app/screens/announcements/use-service-updates.ts
@@ -4,6 +4,7 @@ import { toJS } from "mobx"
 import { useStores } from "../../models"
 import { userLocale } from "../../i18n"
 import { uniq } from "lodash"
+import { collection, getDocs, query, where } from "@react-native-firebase/firestore"
 
 export interface ServiceUpdate {
   expireAt: Date
@@ -16,18 +17,21 @@ export function useServiceUpdates() {
   const { settings, favoriteRoutes } = useStores()
 
   return useQuery("serviceUpdates", async () => {
-    let data: ServiceUpdate[] = []
+    const data: ServiceUpdate[] = []
 
     // Get all the station that the user has notifications enabled for, including their favorite routes
     const favoriteStations = favoriteRoutes.routes.flatMap((route) => [route.originId, route.destinationId])
     const notificationsStations = uniq([...toJS(settings.stationsNotifications), ...favoriteStations])
 
-    const querySnapshot = await firestore
-      .collection("service-updates")
-      .where("expiresAt", ">", new Date())
-      .where("stations", "array-contains-any", [...notificationsStations, "all-stations"])
-      .get()
+    const q = query(
+      collection(firestore, "service-updates"),
+      where("expiresAt", ">", new Date()),
+      where("stations", "array-contains-any", [...notificationsStations, "all-stations"]),
+    )
 
+    const querySnapshot = await getDocs(q)
+
+    // biome-ignore lint/complexity/noForEach: firebase standard (per docs)
     querySnapshot.forEach((doc) => {
       const documentData = doc.data()
       data.push({ ...documentData[userLocale], link: documentData.url } as ServiceUpdate)
