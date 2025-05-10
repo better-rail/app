4535af6375e35e7e3a6f78c3f5c82295a9291adb
Guy Tepper <hey@guytepper.com>
2025-05-10 10:59:51 +0300

    ✨ - Switch stations using a fling gesture (#436)

---
+++ app/screens/planner/planner-slider-wrapper.tsx
@@ -0,0 +1,50 @@
+import { Gesture, GestureDetector } from "react-native-gesture-handler"
+import { isRTL } from "../../i18n"
+import Animated, {
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withSequence,
+  withSpring,
+  withTiming,
+} from "react-native-reanimated"
+import { logAnalyticsEvent } from "../../services/firebase/analytics"
+
+interface FlingGestureWrapperProps {
+  children: React.ReactNode
+  onFling: () => void
+}
+
+/**
+ * A wrapper that allows the user to fling the screen to trigger an action
+ */
+export function FlingGestureWrapper(props: FlingGestureWrapperProps) {
+  const { children, onFling } = props
+  const slideOffset = useSharedValue(0)
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: slideOffset.value }],
+    }
+  })
+
+  const gesture = Gesture.Pan().onEnd((e) => {
+    const direction = isRTL ? -1 : 1
+
+    // Only trigger if the velocity is high enough and in the correct direction
+    if (Math.abs(e.velocityX) > 800 && Math.sign(e.velocityX) === direction) {
+      slideOffset.value = withSequence(
+        withTiming(direction * 12, { duration: 100 }),
+        withSpring(0, { damping: 10, stiffness: 100 }),
+      )
+      runOnJS(onFling)()
+      runOnJS(logAnalyticsEvent)("switch_stations_fling")
+    }
+  })
+
+  return (
+    <GestureDetector gesture={gesture}>
+      <Animated.ScrollView style={animatedStyle}>{children}</Animated.ScrollView>
+    </GestureDetector>
+  )
+}
