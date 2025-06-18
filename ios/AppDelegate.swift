7ade8cc601d6fca10d7521f3a626ade0fc114636
Guy Tepper <hey@guytepper.com>
2025-06-18 20:08:51 +0200

    Update AppDelegate.swift

--- ios/AppDelegate.swift
+++ ios/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
  
     factory.startReactNative(
-      withModuleName: "RnDiffApp",
+      withModuleName: "BetterRail",
       in: window,
       launchOptions: launchOptions
     )
