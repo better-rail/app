16450976553d34c1be1409e337b86eab1087b098
Danny Rehelis <danny@savvy.security>
2025-08-10 23:50:50 +0300

    .

--- app/i18n/i18n.ts
+++ app/i18n/i18n.ts
@@ -103,6 +103,6 @@ export type TxKeyPath = RecursiveKeyOf<DefaultLocale>
 
 type RecursiveKeyOf<TObj extends Record<string, any>> = {
   [TKey in keyof TObj & string]: TObj[TKey] extends Record<string, any>
-  ? `${TKey}` | `${TKey}.${RecursiveKeyOf<TObj[TKey]>}`
-  : `${TKey}`
+    ? `${TKey}` | `${TKey}.${RecursiveKeyOf<TObj[TKey]>}`
+    : `${TKey}`
 }[keyof TObj & string]
