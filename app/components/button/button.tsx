22c49ad9f81676d5bacc7a1df61abd75efa52950
Guy Tepper <hey@guytepper.com>
2025-09-14 18:49:31 +0200

    wip: liquid glass buttons

--- app/components/button/button.tsx
+++ app/components/button/button.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useMemo, ReactNode } from "react"
-import { View, Pressable, TextStyle, ViewStyle, ButtonProps, Platform, ActivityIndicator } from "react-native"
+import { View, Pressable, TextStyle, ViewStyle, ButtonProps, Platform, ActivityIndicator, PlatformColor } from "react-native"
 import { color, fontScale, spacing, typography } from "../../theme"
 import { Text } from "../"
+import { LiquidGlassView, isLiquidGlassSupported } from "@callstack/liquid-glass"
 
 const BUTTON_WRAPPER: ViewStyle = {
   borderRadius: Platform.select({ ios: 12, android: 6 }),
@@ -45,6 +46,12 @@ const SMALL_TEXT: TextStyle = {
   fontWeight: "normal",
 }
 
+const LIQUID_GLASS_STYLE: ViewStyle = {
+  height: 55,
+  padding: spacing[4] * fontScale,
+  borderRadius: 16,
+}
+
 export interface CustomButtonProps extends ButtonProps {
   style?: ViewStyle
   containerStyle?: ViewStyle
@@ -54,6 +61,7 @@ export interface CustomButtonProps extends ButtonProps {
   icon?: ReactNode
   pressedOpacity?: number
   size?: "small"
+  variant?: "primary" | "secondary"
   /**
    * An optional onPress handler that is only called when the button is disabled.
    */
@@ -62,7 +70,7 @@ export interface CustomButtonProps extends ButtonProps {
 
 export const Button = function Button(props: CustomButtonProps) {
   const [isPressed, setIsPressed] = useState(false)
-  const { title, onPress, loading = false, disabled, textStyle, containerStyle, size, icon, style } = props
+  const { title, onPress, loading = false, disabled, textStyle, containerStyle, size, icon, style, variant = "primary" } = props
 
   const PRESSABLE_STYLE = useMemo(() => {
     let modifiedStyles = Object.assign({}, PRESSABLE_BASE, style)
@@ -75,32 +83,56 @@ export const Button = function Button(props: CustomButtonProps) {
     return modifiedStyles
   }, [isPressed, disabled, style])
 
-  return (
-    <View style={[BUTTON_WRAPPER, containerStyle]}>
-      <Pressable
-        style={[PRESSABLE_STYLE, disabled && { backgroundColor: color.disabled }]}
-        onPressIn={() => setIsPressed(true)}
-        onPressOut={() => setIsPressed(false)}
-        android_ripple={{ color: color.primaryLighter }}
-        onPress={(e) => {
-          if (disabled) {
-            if (props.onDisabledPress) props.onDisabledPress()
-          } else {
-            onPress(e)
-          }
-        }}
-      >
-        {loading ? (
-          <ActivityIndicator color={color.whiteText} />
-        ) : (
-          <View style={TEXT_WRAPPER}>
-            {icon}
-            <Text style={[TEXT, textStyle, size === "small" && SMALL_TEXT]} maxFontSizeMultiplier={1.3}>
-              {title}
-            </Text>
-          </View>
-        )}
+  if (isLiquidGlassSupported) {
+    return (
+      <Pressable onPress={onPress}>
+        <LiquidGlassView
+          interactive
+          effect="clear"
+          style={LIQUID_GLASS_STYLE}
+          tintColor={variant === "primary" ? PlatformColor("systemBlue") : PlatformColor("systemPink")}
+        >
+          {loading ? (
+            <ActivityIndicator color={color.whiteText} />
+          ) : (
+            <View style={TEXT_WRAPPER}>
+              {icon}
+              <Text style={[TEXT, textStyle, size === "small" && SMALL_TEXT]} maxFontSizeMultiplier={1.3}>
+                {title}
+              </Text>
+            </View>
+          )}
+        </LiquidGlassView>
       </Pressable>
-    </View>
-  )
+    )
+  } else {
+    return (
+      <View style={[BUTTON_WRAPPER, containerStyle]}>
+        <Pressable
+          style={[PRESSABLE_STYLE, disabled && { backgroundColor: color.disabled }]}
+          onPressIn={() => setIsPressed(true)}
+          onPressOut={() => setIsPressed(false)}
+          android_ripple={{ color: color.primaryLighter }}
+          onPress={(e) => {
+            if (disabled) {
+              if (props.onDisabledPress) props.onDisabledPress()
+            } else {
+              onPress(e)
+            }
+          }}
+        >
+          {loading ? (
+            <ActivityIndicator color={color.whiteText} />
+          ) : (
+            <View style={TEXT_WRAPPER}>
+              {icon}
+              <Text style={[TEXT, textStyle, size === "small" && SMALL_TEXT]} maxFontSizeMultiplier={1.3}>
+                {title}
+              </Text>
+            </View>
+          )}
+        </Pressable>
+      </View>
+    )
+  }
 }
