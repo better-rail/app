aa92ad03423e0574ea0354eb2c968c0f200969cd
Danny Rehelis <danny@savvy.security>
2025-10-02 08:33:37 +0300

    Notifications select stations screen to follow edge-to-edge pattern

--- app/screens/notifications/notifications-select-stations-screen.tsx
+++ app/screens/notifications/notifications-select-stations-screen.tsx
@@ -2,15 +2,16 @@ import { observer } from "mobx-react-lite"
 import { useState } from "react"
 
 import { Pressable, View } from "react-native"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
 import { StationListItem } from "./station-list-item"
 import { color, fontScale, spacing } from "../../theme"
-import { Text } from "../../components"
+import { Screen, Text } from "../../components"
 import { useNavigation } from "@react-navigation/native"
 import { SearchInput } from "../select-station/search-input"
 import { translate } from "../../i18n"
 import { useStores } from "../../models"
 import HapticFeedback from "react-native-haptic-feedback"
-import { useFilteredStations } from "../../hooks"
+import { useFilteredStations, useIsDarkMode } from "../../hooks"
 import { useStations } from "../../data/stations"
 
 import { FlashList } from "@shopify/flash-list"
@@ -19,6 +20,7 @@ import { toJS } from "mobx"
 export const NotificationsSelectStationsScreen = observer(function NotificationsSelectStationsScreen() {
   const navigation = useNavigation()
   const [searchTerm, setSearchTerm] = useState("")
+  const insets = useSafeAreaInsets()
 
   const { settings } = useStores()
   const { stationsNotifications } = settings
@@ -27,6 +29,8 @@ export const NotificationsSelectStationsScreen = observer(function Notifications
   const { filteredStations } = useFilteredStations(searchTerm)
 
   const displayedStations = searchTerm === "" ? stations : filteredStations
+
+  const isDarkMode = useIsDarkMode()
 
   const onSelected = (stationId: string) => {
     HapticFeedback.trigger("impactLight")
@@ -40,44 +44,51 @@ export const NotificationsSelectStationsScreen = observer(function Notifications
   }
 
   return (
-    <View style={{ flex: 1, paddingHorizontal: spacing[3] }}>
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          marginTop: spacing[3],
-          marginBottom: spacing[2],
-          gap: spacing[3],
-        }}
-      >
-        <SearchInput searchTerm={searchTerm} setSearchTerm={setSearchTerm} autoFocus={false} />
-        <Pressable onPress={navigation.goBack}>
-          <Text style={{ color: color.primary }}>
-            {translate("common.done")} ({stationsNotifications.length})
-          </Text>
-        </Pressable>
-      </View>
+    <Screen
+      style={{ flex: 1 }}
+      unsafe={true}
+      statusBarBackgroundColor={isDarkMode ? "#000" : "#fff"}
+      translucent
+    >
+      <View style={{ flex: 1, paddingHorizontal: spacing[3], paddingTop: insets.top }}>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            marginTop: spacing[3],
+            marginBottom: spacing[2],
+            gap: spacing[3],
+          }}
+        >
+          <SearchInput searchTerm={searchTerm} setSearchTerm={setSearchTerm} autoFocus={false} />
+          <Pressable onPress={navigation.goBack}>
+            <Text style={{ color: color.primary }}>
+              {translate("common.done")} ({stationsNotifications.length})
+            </Text>
+          </Pressable>
+        </View>
 
-      <FlashList
-        data={displayedStations}
-        renderItem={({ item, extraData: selectedStations }) => {
-          return (
-            <StationListItem
-              title={item.name}
-              image={item.image}
-              selected={selectedStations.includes(item.id)}
-              onSelect={() => onSelected(item.id)}
-              style={{ marginBottom: spacing[3] }}
-            />
-          )
-        }}
-        keyExtractor={(item) => item.id}
-        showsVerticalScrollIndicator={false}
-        estimatedItemSize={76 * fontScale}
-        keyboardShouldPersistTaps="handled"
-        contentContainerStyle={{ paddingTop: spacing[2], paddingBottom: spacing[5] }}
-        extraData={toJS(stationsNotifications)}
-      />
-    </View>
+        <FlashList
+          data={displayedStations}
+          renderItem={({ item, extraData: selectedStations }) => {
+            return (
+              <StationListItem
+                title={item.name}
+                image={item.image}
+                selected={selectedStations.includes(item.id)}
+                onSelect={() => onSelected(item.id)}
+                style={{ marginBottom: spacing[3] }}
+              />
+            )
+          }}
+          keyExtractor={(item) => item.id}
+          showsVerticalScrollIndicator={false}
+          estimatedItemSize={76 * fontScale}
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ paddingTop: spacing[2], paddingBottom: spacing[5] }}
+          extraData={toJS(stationsNotifications)}
+        />
+      </View>
+    </Screen>
   )
 })
